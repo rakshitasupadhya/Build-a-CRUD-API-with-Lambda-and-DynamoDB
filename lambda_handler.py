fe8da44f6edca021e77acf5174c57bc2d88762f6
Rakshitha <107621546+rakshitasupadhya@users.noreply.github.com>
2023-11-24 12:58:32 +0530

    Create lambda_handler.py

---
+++ lambda_handler.py
@@ -0,0 +1,56 @@
+import boto3
+import json
+
+dynamodb = boto3.resource('dynamodb')
+tableName = "CRUD-DB"
+table = dynamodb.Table(tableName)
+
+def lambda_handler(event, context):
+    print('Line11', event)
+    id= event['queryStringParameters']['ID']
+    body = None
+    statusCode = 200
+    headers = {
+        "Content-Type": "application/json",
+    }
+    try:
+        if event['routeKey'] == "DELETE /items/{ID}":
+            response= table.delete_item(
+                Key={
+                    "ID": id
+                }
+            )
+            body = f"Deleted item {event['queryStringParameters']['ID']}"
+
+        elif event["routeKey"] == "GET /items/{ID}":
+            response = table.get_item(
+                Key={
+                    "ID": id
+                }
+            ).get('Item')
+            body = response
+            
+        elif event["routeKey"] == "GET /items":
+            response = table.scan(
+            )
+            body = response.get("Items", [])
+            
+        elif event['routeKey'] == "PUT /items":
+            requestJSON = json.dumps(event['queryStringParameters'])
+            request_data_str = json.loads(requestJSON)
+            table.put_item(
+                
+                Item=request_data_str
+            )
+            body =f"Item added successfully {event['queryStringParameters']['ID']}"
+
+    except Exception as e:
+        statusCode = 500
+        body = str(e)
+
+    return {
+        "statusCode": statusCode,
+        "headers": headers,
+        "body": json.dumps(body)
+    }
+
